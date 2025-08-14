f3a80fb55d5fccfc6e6259d4ed56fa84add4623b
KSHITIJ VERMA <159154144+Kshitijrocks7@users.noreply.github.com>
2025-08-14 19:03:35 +0530

    Update solve_rubiccs_cube.py

--- solve_rubiccs_cube.py
+++ solve_rubiccs_cube.py
@@ -141,5 +141,7 @@ def solve(cube):
     values = ""
     values = values.join(decode_postion(cube))
     solution = kociemba.solve(values)
+    print('solution is \n' + solution)
+
 
     return solution
